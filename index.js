6b27e4f3d349b1f741cf205c2d9106edbd728b9c
Thamu <thamiendlovu00@gmail.com>
2022-03-18 10:45:16 +0200

    Updated the task service

--- index.js
+++ index.js
@@ -1,124 +1,81 @@
-//start drop down
+var form = document.getElementById("inputForm");
+form.addEventListener("submit", saveTask);
+
+function fetchTasks() {
+    resetLists();
+  var tasks = JSON.parse(localStorage.getItem("notes"));
+  if(!tasks)
+  return;
+  tasks.forEach((task) => {
+    var title = task.title;
+    var description = task.description;
+    var status = task.status;
+    var card = document.createElement("li");
+    card.classList.add("task");
+    card.classList.add("fill");
+
+    if (title && description) {
+      card.innerHTML = `
+                                <h3>${title}</h3>
+                                <p>${description}</p>
+                                <button class="del">Delete</button>`;
+
+      var notesElem = document.getElementById(status);
+      notesElem.appendChild(card);
+    }
+  });
+}
+
+function resetLists()
+{
+    document.getElementById('To Do').innerHTML='';
+    document.getElementById('In Progress').innerHTML='';
+    document.getElementById('Done').innerHTML='';
+}
+
+
+function saveTask(e) {
+  var title = document.getElementById("title").value;
+  var description = document.getElementById("description").value;
+  var status = selectedValue;
+
+  if(title=="")
+  return;
+  var task = {
+    title: title,
+    description: description,
+    status: status,
+  };
+  var tasks =(localStorage.getItem("notes")==null) ? []: JSON.parse(localStorage.getItem("notes"));
+  tasks.push(task);
+  localStorage.setItem('notes', JSON.stringify(tasks));
+  fetchTasks();
+
+  e.preventDefault();
+}
+
+
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 
 const optionsList = document.querySelectorAll(".option");
+var selectedValue = selected.innerHTML.trim();
 
-selected.addEventListener("click", ()=>{
-    optionsContainer.classList.toggle("active");
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
 });
 
+//  when we select on any of the options we want the text to be displayed over select category
+// and remove the active class from
+// add event listener for all options in our list
 
-// when we select on any of the options we want the text to be displayed over select category
-//and remove the active class from optionscontainer
-//add event listener for all options in our list
-
-optionsList.forEach(o =>{
-    o.addEventListener("click", ()=>{
-        selected.innerHTML=o.querySelector("label").innerHTML;
-      //  selValue=selected.innerHTML;
-        
-    
-    optionsContainer.classList.remove("active");
+optionsList.forEach((o) => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      selectedValue = selected.innerHTML.trim();
+      //   console.log(selValue.trim());
+  
+      optionsContainer.classList.remove("active");
     });
-    
-});
-//end drop down
-
-
-
-let submit=document.querySelector(".submit");
-let title=document.getElementById("title");
-let desc=document.getElementById("desc");
-//let select=document.getElementById("select");//
-let notesElem=document.querySelector(".tasks1");
-let notes=JSON.parse(localStorage.getItem("tasks1"));
-if(notes){
-    notes.forEach(element=>{
-        addNotes(element)
-    })
-}
-console.log(title,desc);
-submit.addEventListener("click",(e)=>{
-     e.preventDefault();
-     addNotes()   
-    }) 
-
-
-function addNotes(obj){
-    let titleVal=title.value;
-    let descVal=desc.value;
-     let selVal=selected.innerHTML;
-  //  let selectVal=select.value;//
-
-    if(obj){
-        titleVal=obj.title;
-        descVal=obj.desc;
-        //console.log(descVal);
-    //    selectVal=obj.select;//
-
-
-    }
-    //let statusVal=staetus.value;
-
-
-let card=document.createElement('li');
-card.classList.add('task');
-card.classList.add('fill');
-
-
-if(titleVal ){
-card.innerHTML=`
-<h3>${titleVal }</h3>
-            <p>
-        ${descVal}
-            </p>
-            <p>
-        ${selVal}
-            </p>
-            
-              <button class="del">Delete</button>
-                      
-
-` 
-let fr ="mkhomo";
-let jj="mkhomo";
-
-let newValae=selVal.trim();
-
-let selectV=newValae.length;
-if(selectV===11){
- notesElem.appendChild(card);
- updateLs();}
-if(selectV===4){
-    notesElem2.appendChild(card);
-    updateLs();}
-   
-if(selectV===5){
-        notesElem1.appendChild(card);
-        updateLs();}
-       
-    
-}
-
-
-let clear =card.querySelector(".del");
-clear.addEventListener("click",()=>{
-    card.remove();
-})
-}
-
-
-function updateLs(){
-
-    let card =document.querySelectorAll(".li");
-    let arr=[];
-    card.forEach(element=>{
-        arr.push({
-            title:element.children[0].innerText,
-            desc:element.children[1].innerText,
-            //select:element.children[2].innerText,
-        })
-    });
-    localStorage.setItem("notes",JSON.stringify(arr));
-}
+  });
+  
